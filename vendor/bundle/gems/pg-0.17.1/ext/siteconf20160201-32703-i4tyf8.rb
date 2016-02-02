0739d52c9af3469627a3326ac9c85bd1ae7cc986
Rmandal <Rpgraf@gmail.com>
2016-02-01 18:46:38 -0600

    initialize reposetory

---
+++ vendor/bundle/gems/pg-0.17.1/ext/siteconf20160201-32703-i4tyf8.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160201-32703-1kp8jdc"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
